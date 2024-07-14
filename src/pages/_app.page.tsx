57a6822896c3d10a3943aba4532bfa714d04cc4d
wo-o29 <woogur29@gmail.com>
2024-07-14 23:01:58 +0900

    feat: GA 추가

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import GlobalStyle from "../styles/Global";
 import { ThemeProvider } from "../context/ThemeContext";
 import ThemeToggle from "../components/ThemeToggle";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeToggle />
       <Component {...pageProps} />
       <SpeedInsights />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
     </ThemeProvider>
   );
 }
