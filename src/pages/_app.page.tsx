9b4f0fcef97a2adb5a826967c685e90986013cb1
wo-o29 <woogur29@gmail.com>
2024-07-14 22:19:54 +0900

    feat: insights 추가

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,7 @@ import "../styles/reset.css";
 import GlobalStyle from "../styles/Global";
 import { ThemeProvider } from "../context/ThemeContext";
 import ThemeToggle from "../components/ThemeToggle";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       <ThemeToggle />
       <Component {...pageProps} />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
